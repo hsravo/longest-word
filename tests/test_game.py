eb9b3c7fef39896bcc6483d8a3fc6ede12e2c909
Steph Ravoninjatovo <hsravo1@gmail.com>
2023-10-11 13:50:32 +0000

    TDD: Check that attempt exists in the English dictionary

--- tests/test_game.py
+++ tests/test_game.py
@@ -3,7 +3,7 @@ from longest_word.game import Game
 
 class TestGame:
     def test_game_initialization(self):
-
+        """A grid which size is 9 and is a list should be valid"""
         game = Game()
         grid = game.grid
 
@@ -11,7 +11,7 @@ class TestGame:
         assert type(grid) == list
 
     def test_is_valid(self):
-
+        """A word which letters are in the grid should be valid"""
         game = Game()
         game.grid = ['V', 'I', 'F', 'J', 'L', 'G', 'Z', 'E', 'A']
         word = 'AGILE'
@@ -21,10 +21,16 @@ class TestGame:
         assert game.is_valid(word=fake_word) is False
 
     def test_is_a_word(self):
-
+        """A word that exists in english directory should be valid"""
         game = Game()
         word = "VOLLEYBALL"
         fake_word = "NOTVOLLEYBALL"
 
         assert game.is_a_word(word=word) is True
         assert game.is_a_word(word=fake_word) is False
+
+    def test_unknown_word_is_invalid(self):
+        """A word that is not in the english directory should no be valid"""
+        new_game = Game()
+        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+        assert new_game.is_valid('FEUN') is False
